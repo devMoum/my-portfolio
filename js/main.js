2653dd244f3b0a2aea53644189de8285f6db83e7
unknown <piradoxa@gmail.com>
2024-01-22 17:56:03 +0100

    ADDED SCROLL TO TOP BTN

--- js/main.js
+++ js/main.js
@@ -2,10 +2,18 @@
 const burger = document.getElementById("burger");
 const navBar = document.getElementById("hidden");
 burger.onclick = ()=>{
-    if (navBar.hasAttribute("class")){
-        navBar.removeAttribute("class");
-    }else{
-        navBar.setAttribute("class", "active");
-    }  
+    navBar.hasAttribute("class") ? navBar.removeAttribute("class"): navBar.setAttribute("class", "active");
 };
 // end burger menu 
+// start scrool totop 
+let arrow = document.querySelector(".upscroll");
+window.onscroll = function () {
+    this.scrollY >= 1000 ? arrow.classList.add("show"): arrow.classList.remove("show"); 
+}
+arrow.onclick = ()=> {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    })
+}
+// end scroll to top 
